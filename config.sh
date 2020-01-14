160ddb23eb0c2541aeb75033097b1ea806295432
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-01-14 17:51:10 +0100

    Configure hostname

--- config.sh
+++ config.sh
@@ -10,6 +10,11 @@ echo
 echo 'IMG_NAME=onecable' | tee -a config
 echo 'TIMEZONE_DEFAULT=Europe/Amsterdam' | tee -a config
 echo 'LOCALE_DEFAULT="en_US.UTF-8"' | tee -a config
+
+echo
+echo 'HOSTNAME="bramble"' | tee -a config
+echo
+
 echo "FIRST_USER_NAME=\"$FIRST_USER_NAME\"" | tee -a config
 echo "FIRST_USER_PASS=\"********\""
 echo "FIRST_USER_PASS=\"$FIRST_USER_PASS\"" >> config
