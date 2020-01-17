f0d91fd0c7bd01f77c33482fdcae5cae6a669cf5
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-01-17 01:13:44 +0100

    Don't install docker or k3s

--- stage2/04-onecable/01-run.sh
+++ stage2/04-onecable/01-run.sh
@@ -15,18 +15,4 @@ on_chroot << EOF
   # need to rehash because of https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=923479
   # found solution at https://github.com/balena-io-library/base-images/issues/562#issuecomment-511506804
   c_rehash /etc/ssl/certs
-
-  # install docker
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-  if [ ! -f /usr/bin/docker ] ; then
-      curl -sSL https://get.docker.com | sh
-  fi
-
-  systemctl disable docker
-  systemctl disable containerd
-
-  # install k3s
-  # can't use install script, as systemd is not recognized
-  # installing binary manually
-  curl -sSL https://github.com/rancher/k3s/releases/download/v1.17.0%2Bk3s.1/k3s-arm64 > /usr/local/bin/k3s
 EOF
