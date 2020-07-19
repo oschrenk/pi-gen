32c3eb55092c83642ab0430da890039a2135016a
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-07-19 17:39:26 +0200

    Simplify default interface handling

--- find.sh
+++ find.sh
@@ -1,14 +1,16 @@
-#!/bin/bash -eu
-if [ -n "${1-}" ]
-then
-  IP=$(ifconfig $INTERFACE inet | grep inet | awk '{print $2}')
-  INTERFACE=$1
-else
-  IP=$(route get google.com | grep gateway | awk -F  ": " '{print $2}')
-  INTERFACE=$(route get google.com | grep interface | awk -F  ": " '{print $2}')
-fi
+#!/usr/bin/env bash
+set -euo pipefail
+
+DEFAULT_INTERFACE=$(route get google.com | grep interface | awk -F  ": " '{print $2}')
+
+INTERFACE=${1:-$DEFAULT_INTERFACE}
+
+IP=$(ifconfig $INTERFACE inet | grep inet | awk '{print $2}')
 HEX_MASK=$(ifconfig $INTERFACE inet | grep netmask | awk '{print $4}' | cut -c 3- | tr [a-f] [A-F])
 MASK=$(echo "obase=2; ibase=16; $HEX_MASK" | bc | tr -cd '1' | wc -c | awk '{print $1}')
 NET=$(echo "$(echo $IP | cut -d '.' -f 1-3).0/$MASK")
+
+echo "Using interface : $INTERFACE"
+echo "Using IP : $IP"
 echo "Scanning net: $NET"
 sudo nmap -sn "$NET" | grep -B 2 Raspberry | grep "report for" | cut -c 22-
